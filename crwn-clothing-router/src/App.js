ef5cc4c87c53f59f685e2811216d4413cc2633d1
wolf790206 <85617560+wolf790206@users.noreply.github.com>
2024-02-19 20:13:19 +0800

    updata
    Signed-off-by: wolf790206 <85617560+wolf790206@users.noreply.github.com>

---
+++ crwn-clothing-router/src/App.js
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+
+const Shop = () => {
+	return <h1>I'm the shop page</h1>;
+};
+
+const App = () => {
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<Navigation />}
+			>
+				<Route
+					index
+					element={<Home />}
+				/>
+				<Route
+					path="shop"
+					element={<Shop />}
+				/>
+			</Route>
+		</Routes>
+	);
+};
+export default App;
