4ef0138a6c82a18020312c298044088bf20c940a
wolf790206 <85617560+wolf790206@users.noreply.github.com>
2024-02-20 12:25:33 +0800

    updata new page
    Signed-off-by: wolf790206 <85617560+wolf790206@users.noreply.github.com>

---
+++ crwn-clothing-firebase-lesson3/src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,20 @@
+import { Await } from 'react-router-dom';
+import {
+	signInWithGooglePopup,
+	createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
+const SignIn = () => {
+	const logGoogleUser = async () => {
+		const { user } = await signInWithGooglePopup();
+		const userDocRef = await createUserDocumentFromAuth(user);
+	};
+	return (
+		<div>
+			<h1>Sign In Page</h1>
+			<button onClick={logGoogleUser}>Sign In with google popup</button>
+		</div>
+	);
+};
+
+export default SignIn;
