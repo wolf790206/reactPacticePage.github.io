87700b71a44777062ab2b0272f4e82272b21c024
wolf790206 <85617560+wolf790206@users.noreply.github.com>
2024-02-23 01:47:03 +0800

    updata new page
    Signed-off-by: wolf790206 <85617560+wolf790206@users.noreply.github.com>

---
+++ crwn-clothing-firebase-lesson8/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<HashRouter>
+			<UserProvider>
+				<App />
+			</UserProvider>
+		</HashRouter>
+	</React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
